72fd83aade8ca1b5bcea3b24f5321576a9b1216d
Barathmurugan367 <barath.g@radicalstart.com>
2025-06-28 11:24:18 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,23 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "FSPagerView",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v10)
     ],
     products: [
-        .library(name: "FSPagerView", targets: ["FSPagerView"]),
+        .library(
+            name: "FSPagerView",
+            targets: ["FSPagerView"]),
     ],
     targets: [
-        .target(name: "FSPagerView", path: "Sources", exclude: ["FSPagerViewObjcCompat.h", "FSPagerViewObjcCompat.m"]),
-    ],
-    swiftLanguageVersions: [.v5]
+        .target(
+            name: "FSPagerView",
+            path: "Sources",
+            exclude: [],
+            resources: [.process("FSPagerView.bundle")]
+        )
+    ]
 )
+
